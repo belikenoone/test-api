4044fc80ee770a1549734540522b7b0bf8c4d4b0
belikenoone <125812096+belikenoone@users.noreply.github.com>
2024-12-12 21:49:26 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import {express} from "express"
+import cors from "cors"
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.json({message:"Hello"})
+})
+app.listen(8080,()=>{
+    console.log("App is running on port 8080")
+})
